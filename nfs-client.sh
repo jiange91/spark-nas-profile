54e549d9194e1088c44a3927388219671b1a7ef0
Zijian Jim <zijianjim@outlook.com>
2021-12-06 00:07:25 -0800

    cloud lab nfs

--- nfs-client.sh
+++ nfs-client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y nfs-common
-sudo mkdir -p /var/nfs
+sudo mkdir -p /nfs
 master_ip=$(gethostip -d master);
-sudo mount $master_ip:/var/nfs /var/nfs
+sudo mount $master_ip:/nfs /nfs
